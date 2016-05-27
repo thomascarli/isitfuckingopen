82ec37954672fa6a8a87287990510516dff05f59
Sebastian Valdez <valdez.sebastian4@gmail.com>
2016-05-27 12:06:30 -0700

    added gems add new client instance

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,4 +1,13 @@
 class LocationsController < ApplicationController
   def index
+  	@client = GooglePlaces::Client.new(ENV['google_access_key'])
+	@spots = @client.spots(-33.8670522, 151.1957362, :types => 'restaurant')
   end
+
+  def show
+
+  end
+
+
+
 end

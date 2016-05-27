cc108f361500144b2425fb991618c57377b9a4a1
Thomas Carli <thomas@procore.com>
2016-05-27 21:18:59 +0000

    build ajax methods and controller actions for mvp

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,31 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+function generate_location_data(location_name) {
+  gps_data = retrieve_gps_data();
+
+  $.ajax({
+    url: "/generate_location_data",
+    type: "POST",
+    data: { "location_name": location_name, "gps_data": gps_data },
+    success: function(json) {
+      var is_it_open = json.is_it_open.toString();
+      $('.location-open-data').text(is_it_open);
+    }
+  });
+}
+
+function retrieve_gps_data() {
+  // Eventually this will be browsers location data -TC
+  return {"lat": -33.8670522, "lon": 151.1957362}
+}
+
+$( document ).on('ready page:load', function() {
+
+  $(".location-search-submit").on('click', function(e) {
+    var location_name = $(".location-search-input").val();
+    generate_location_data(location_name);
+  });
+
+});

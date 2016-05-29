0af69bcf3d6b84c42aae12fcecf0f24cb75b1334
Thomas Carli <thomas@procore.com>
2016-05-29 01:17:59 +0000

    update ssl force

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,7 @@ module Isitfuckingopen
     # config.i18n.default_locale = :de
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.force_ssl = true
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
